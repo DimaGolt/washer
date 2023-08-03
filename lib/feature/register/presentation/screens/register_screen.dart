3a7602ec25e7fefb4f6bce6c3f306d14fa123b86
Dima_Golt <dimagolt@gmail.com>
2023-08-03 22:01:19 +0200

    add register validators

--- lib/feature/register/presentation/screens/register_screen.dart
+++ lib/feature/register/presentation/screens/register_screen.dart
@@ -4,13 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:washu/app/theme.dart';
 import 'package:washu/shared/data/app_auth_repository.dart';
+import 'package:washu/shared/utils/string_regexp.dart';
 import 'package:washu/shared/widgets/logo_widget.dart';
 
 import '../../../../shared/widgets/background_wave_container.dart';
 
 @RoutePage()
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  RegisterScreen({super.key});
+
+  final GlobalKey<FormState> formKey = GlobalKey();
+  final GlobalKey<FormFieldState> emailKey = GlobalKey();
+  final GlobalKey<FormFieldState> passwordKey = GlobalKey();
+  final GlobalKey<FormFieldState> repeatKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -43,30 +49,49 @@ class RegisterScreen extends StatelessWidget {
 
   _form(BuildContext context) {
     return Form(
+      key: formKey,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-
-          TextField(
+          TextFormField(
+            key: emailKey,
             style: const TextStyle(color: Color(0xFFB1E3F9), fontSize: 20),
             keyboardType: TextInputType.emailAddress,
             textAlign: TextAlign.center,
             decoration: Theme.of(context).emailDecoration(),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'E-mail can\'t be empty';
+              }
+              return value.matchesEmail() ? null : 'E-mail invalid';
+            },
           ),
-          TextField(
+          TextFormField(
+            key: passwordKey,
             style: const TextStyle(color: Color(0xFFB1E3F9), fontSize: 20),
             textAlign: TextAlign.center,
             decoration: Theme.of(context).passwordDecoration(
               suffixVisible: true,
               onTap: () {
                 AppAuthRepository().signOut();
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Password can\'t be empty';
               }
-            ),
+              return value.matchesPassword() ? null : 'Password invalid';
+            },
           ),
-          TextField(
+          TextFormField(
+            key: repeatKey,
             style: const TextStyle(color: Color(0xFFB1E3F9), fontSize: 20),
             textAlign: TextAlign.center,
             decoration: Theme.of(context).passwordDecoration(suffixVisible: false),
+            validator: (value) {
+              if (value != passwordKey.currentState!.value) {
+                return 'Passwords aren\'t the same';
+              }
+              return null;
+            },
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
