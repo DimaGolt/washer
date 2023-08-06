35fc1efa8841b435d09bae0302fd20f1b7677570
Dima_Golt <dimagolt@gmail.com>
2023-08-07 00:25:47 +0200

    change imports

--- lib/feature/register/presentation/screens/register_screen.dart
+++ lib/feature/register/presentation/screens/register_screen.dart
@@ -1,4 +1,4 @@
-import 'package:auto_route/annotations.dart';
+import 'package:washu/app/router.dart';
 import 'package:bloc_widgets/bloc_widgets.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
